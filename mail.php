6f87090bb42ecaec91e684deb6f36e0d0b6f94a3
ThandoXalabile <69629107+ThandoXalabile@users.noreply.github.com>
2022-08-23 15:19:27 +0200

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$number= $_POST['number'];
+$message= $_POST['message'];
+$to = "umkhonto";
+$subject = "Mail From ";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message. "\r\n  number = " . $number;
+$headers = "From: thandoxalabile@gmail.com" . "\r\n" .
+"CC: lulamani.baloyi@gmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
