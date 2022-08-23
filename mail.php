18a48ff0d00493441bf2f177c297ddf965cd7bd9
ThandoXalabile <69629107+ThandoXalabile@users.noreply.github.com>
2022-08-23 15:36:35 +0200

    Add files via upload

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $message= $_POST['message'];
 $to = "umkhonto";
 $subject = "Mail From ";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message. "\r\n  number = " . $number;
-$headers = "From: thandoxalabile@gmail.com" . "\r\n" .
+$headers = "From: noreply@codeconia.com" . "\r\n" .
 "CC: lulamani.baloyi@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
